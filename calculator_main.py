8c6b65680c39935a9d84ea65ecb4980715c98db7
dmridmrir <wjdrms7169@gmail.com>
2023-12-01 00:02:12 +0900

    새로운 버튼 추가와 위치 조정

--- calculator_main.py
+++ calculator_main.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
 
 class Main(QDialog):
     def __init__(self):
@@ -10,7 +11,7 @@ class Main(QDialog):
         main_layout = QGridLayout()
 
         # 수식과 답을 나타낼 QLineEdit 위젯 생성
-        self.equation = QLineEdit("")
+        self.equation = QLineEdit()
         self.equation.setReadOnly(True)  # 읽기 전용으로 설정하여 사용자가 직접 편집하지 못하도록 함
 
         # 수식 및 버튼을 그리드 레이아웃에 추가
@@ -25,20 +26,16 @@ class Main(QDialog):
             number_button_dict[number].clicked.connect(lambda state, num=number:
                                                self.number_button_clicked(num))
             x, y = divmod(i, 3)
-            main_layout.addWidget(number_button_dict[number], x+1, y)
+            main_layout.addWidget(number_button_dict[number], x+4, y)
 
         number_button_dict[0] = QPushButton("0")
         number_button_dict[0].clicked.connect(lambda state, num=0: self.number_button_clicked(num))
-        main_layout.addWidget(number_button_dict[0], 4, 1)
+        main_layout.addWidget(number_button_dict[0], 7, 1)
 
         # 소숫점 버튼과 00 버튼을 그리드 레이아웃에 추가하고 시그널 설정
         button_dot = QPushButton(".")
         button_dot.clicked.connect(lambda state, num=".": self.number_button_clicked(num))
-        main_layout.addWidget(button_dot, 4, 2)
-
-        button_double_zero = QPushButton("00")
-        button_double_zero.clicked.connect(lambda state, num="00": self.number_button_clicked(num))
-        main_layout.addWidget(button_double_zero, 4, 0)
+        main_layout.addWidget(button_dot, 7, 2)
 
         # 사칙연산 버튼 생성
         button_plus = QPushButton("+")
@@ -53,26 +50,42 @@ class Main(QDialog):
         button_division.clicked.connect(lambda state, operation="÷": self.button_operation_clicked(operation))
 
         # 사칙연산 버튼을 그리드 레이아웃에 추가
-        main_layout.addWidget(button_plus, 4, 3)
-        main_layout.addWidget(button_minus, 3, 3)
-        main_layout.addWidget(button_product, 2, 3)
-        main_layout.addWidget(button_division, 1, 3)
+        main_layout.addWidget(button_plus, 6, 3)
+        main_layout.addWidget(button_minus, 5, 3)
+        main_layout.addWidget(button_product, 4, 3)
+        main_layout.addWidget(button_division, 3, 3)
 
         # =, clear, backspace 버튼 생성
         button_equal = QPushButton("=")
-        button_clear = QPushButton("Clear")
-        button_backspace = QPushButton("Backspace")
+        button_clear1 = QPushButton("CE")
+        button_clear2 = QPushButton("C")
+        button_backspace = QPushButton("◁")
+
+        # % 1/x x^2 sqrt(x) +/- 키 생성
+        button_mod = QPushButton("%")
+        button_fountain = QPushButton("1/x")
+        button_square = QPushButton("x²")
+        button_sqrt = QPushButton("√x")
+        button_sign = QPushButton("+/-")
+
+        #새로 생성한 버튼 추가
+        main_layout.addWidget(button_mod, 2, 0)
+        main_layout.addWidget(button_fountain, 3, 0)
+        main_layout.addWidget(button_square, 3, 1)
+        main_layout.addWidget(button_sqrt, 3,2)
+        main_layout.addWidget(button_sign, 7, 0)
 
         # =, clear, backspace 버튼 클릭 시 시그널 설정
         button_equal.clicked.connect(self.button_equal_clicked)
-        button_clear.clicked.connect(self.button_clear_clicked)
+        button_clear1.clicked.connect(self.button_clear_clicked)
+        button_clear2.clicked.connect(self.button_clear_clicked)        
         button_backspace.clicked.connect(self.button_backspace_clicked)
 
         # =, clear, backspace 버튼을 그리드 레이아웃에 추가
-        main_layout.addWidget(button_clear, 5, 0)
-        main_layout.addWidget(button_backspace, 5, 1)
-        main_layout.addWidget(button_equal, 5, 2)
-
+        main_layout.addWidget(button_clear1, 2, 1)
+        main_layout.addWidget(button_clear2, 2, 2)
+        main_layout.addWidget(button_backspace, 2, 3)
+        main_layout.addWidget(button_equal, 7, 3)
         # 위젯을 설정한 레이아웃으로 설정
         self.setLayout(main_layout)
         self.show()
@@ -80,24 +93,66 @@ class Main(QDialog):
     #################
     ### functions ###
     #################
+    arr = [None]
+    i = 0
+
     def number_button_clicked(self, num):
         current_equation = self.equation.text()
         self.equation.setText(current_equation + str(num))
 
     def button_operation_clicked(self, operation):
         current_equation = self.equation.text()
-        self.equation.setText(current_equation + operation)
+
+        # 현재 연산자와 수식을 문자열로 저장
+        self.arr.extend([str(current_equation), str(operation)])
+
+        # 다음 연산자로 이동
+        self.i += 1
+
+        # 현재 수식 초기화
+        self.equation.setText("")
+
+    def evaluate_expression(self, expression):
+        # 사칙연산 우선 순위를 고려하여 수식 계산
+        tokens = expression.replace(' ', '')
+        result = float(tokens[0])
+        current_operator = None
+
+        for token in tokens[1:]:
+            if token.isdigit() or token == '.':
+                if current_operator == '+':
+                    result += float(token)
+                elif current_operator == '-':
+                    result -= float(token)
+                elif current_operator == '*':
+                    result *= float(token)
+                elif current_operator == '÷':
+                    result /= float(token)
+            else:
+                current_operator = token
+
+        return result
 
     def button_equal_clicked(self):
         equation = self.equation.text()
         try:
-            result = eval(equation)
+            # 현재 수식과 이전 수식을 평가하여 결과를 표시
+            self.arr.append(str(equation))
+
+            result = self.evaluate_expression(''.join(filter(None, self.arr)))
             self.equation.setText(str(result))
+
+            # 결과를 다시 수식 배열에 저장
+            self.arr = [str(result)]
+
         except Exception as e:
-            self.equation.setText("Error")
+            error_message = f"에러: {e}"
+            QMessageBox.critical(self, "에러", error_message)
+            self.equation.setText("")
 
     def button_clear_clicked(self):
         self.equation.setText("")
+        self.arr = []
 
     def button_backspace_clicked(self):
         current_equation = self.equation.text()
